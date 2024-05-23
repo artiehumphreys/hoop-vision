f20e8e262faa75c52ab6b930864d33cc31199e8d
artiehumphreys <artieh2003@gmail.com>
2024-05-23 16:31:05 -0400

    Refactored code

--- detect_players.py
+++ detect_players.py
@@ -32,35 +32,36 @@ def detect_players_with_roboflow(image_path):
     model_id = 1
     confidence = 0.6
     iou_thresh = 0.5
+
+    # https://inference.roboflow.com/quickstart/run_model_on_image/#run-inference-on-a-v1-route
     api_url = f'https://detect.roboflow.com/{project_id}/{model_id}?api_key={api_key}&confidence={confidence}&overlap={iou_thresh}'
 
     response = requests.post(api_url, data=img_str, headers={"Content-Type": "application/json"})
 
-    #shot logic:
-    # - keep track of latest player within certain distance of ball
-    # - if ball is a certain threshold above rim, it is a shot
+    # https://universe.roboflow.com/ownprojects/basketball-w2xcw/model/1
     shot = False
-    if response.status_code == 200:
-        predictions = response.json()
-        print(predictions)
-        for prediction in predictions['predictions']:
-            if prediction['class'] == 'ball':
-                ball_y = prediction['y']
-            elif prediction['class'] == 'rim':
-                rim_y = prediction['y']
-            width = int(prediction['width'])
-            height = int(prediction['height'])
-            start_x = int(prediction['x'] + width/2)
-            start_y = int(prediction['y'] + height/2)
-            cv2.rectangle(image, (start_x, start_y), (int(start_x - width), int(start_y - height)), (0, 255, 0), 2)
-            cv2.putText(image, prediction['class'] + " " + str(round(prediction['confidence'], 2)), (int(start_x - width), int(start_y - height) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        if rim_y and ball_y:
-            shot = detect_shot.detect_shot(rim_y, ball_y)
-        cv2.putText(image, "Shot: " + str(shot), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow("Original Image with Detected Players", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
+    if response.status_code != 200:
         print("Failed to get a response from RoboFlow API")
+        return
+    
+    predictions = response.json()
+    print(predictions)
+    for prediction in predictions['predictions']:
+        if prediction['class'] == 'ball':
+            ball_y = prediction['y']
+        elif prediction['class'] == 'rim':
+            rim_y = prediction['y']
+        width = int(prediction['width'])
+        height = int(prediction['height'])
+        start_x = int(prediction['x'] + width/2)
+        start_y = int(prediction['y'] + height/2)
+        cv2.rectangle(image, (start_x, start_y), (int(start_x - width), int(start_y - height)), (0, 255, 0), 2)
+        cv2.putText(image, f'{prediction['class']} {str(round(prediction['confidence'], 2))}', (int(start_x - width), int(start_y - height) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    if rim_y and ball_y:
+        shot = detect_shot.detect_shot(rim_y, ball_y)
+    cv2.putText(image, f'Shot: {str(shot)}', (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.imshow("Original Image with Detected Players", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-detect_players_with_roboflow('data/frame125.jpg')
+detect_players_with_roboflow('data/frame50.jpg')
