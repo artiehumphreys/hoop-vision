098fb5d044ba1d78b32251fd70b250bf125423f0
artiehumphreys <artieh2003@gmail.com>
2024-05-23 09:38:55 -0400

    Made mask for the hardwood court. There is still some noise in background

--- get_field_outline.py
+++ get_field_outline.py
@@ -4,10 +4,22 @@ import cv2
 
 def get_field_outline(path : str, precision : int = 5):
     image = cv2.imread(path)
+    if image is None:
+        print("Couldn't load image")
+        return
 
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower_wood = np.array([10, 40, 50])
+    upper_wood = np.array([30, 255, 255])
+
+    hardwood_mask = cv2.inRange(hsv_image, lower_wood, upper_wood)
+
+    cv2.imshow("Mask", hardwood_mask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
 
 get_frames.extract_frames(5)
-get_field_outline('data/frame5.jpg')
+get_field_outline('data/frame95.jpg')
     
