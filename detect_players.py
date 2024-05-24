4909f56486345379d08d594553296e6e72826de1
artiehumphreys <artieh2003@gmail.com>
2024-05-24 10:42:49 -0400

    Working on modelling players on 2D map of court

--- detect_players.py
+++ detect_players.py
@@ -5,15 +5,32 @@ from io import BytesIO
 from dotenv import load_dotenv
 import os
 import cv2
-
 import detect_shot
 
 load_dotenv()
 api_key = os.getenv('ROBOFLOW_API_KEY')
 
-def detect_players_with_roboflow(image_path):
+img_width = 0
+img_height = 0
+
+def make_request(img_str, project_id : str, model_id : int = 1):
+    confidence = 0.5
+    iou_thresh = 0.5
+    # https://inference.roboflow.com/quickstart/run_model_on_image/#run-inference-on-a-v1-route
+    api_url = f'https://detect.roboflow.com/{project_id}/{model_id}?api_key={api_key}&confidence={confidence}&overlap={iou_thresh}'
+
+    response = requests.post(api_url, data=img_str, headers={"Content-Type": "application/json"})
+
+    if response.status_code != 200:
+        print("Failed to get a response from RoboFlow API")
+        return
+    return response.json()
+
+
+def detect_players_with_roboflow(image_path : str):
     rim_y = None
     ball_y = None
+    player_positions = []
     image = cv2.imread(image_path)
     if image is None:
         print("Couldn't load image")
@@ -30,38 +47,29 @@ def detect_players_with_roboflow(image_path):
 
     project_id = 'basketball-w2xcw'
     model_id = 1
-    confidence = 0.6
-    iou_thresh = 0.5
 
-    # https://inference.roboflow.com/quickstart/run_model_on_image/#run-inference-on-a-v1-route
-    api_url = f'https://detect.roboflow.com/{project_id}/{model_id}?api_key={api_key}&confidence={confidence}&overlap={iou_thresh}'
-
-    response = requests.post(api_url, data=img_str, headers={"Content-Type": "application/json"})
+    predictions = make_request(img_str, project_id, model_id)
 
     # https://universe.roboflow.com/ownprojects/basketball-w2xcw/model/1
     shot = False
-    if response.status_code != 200:
-        print("Failed to get a response from RoboFlow API")
-        return
     
-    predictions = response.json()
-    print(predictions)
     for prediction in predictions['predictions']:
-        if prediction['class'] == 'ball':
-            ball_y = prediction['y']
-        elif prediction['class'] == 'rim':
-            rim_y = prediction['y']
         width = int(prediction['width'])
         height = int(prediction['height'])
         start_x = int(prediction['x'] + width/2)
         start_y = int(prediction['y'] + height/2)
+        if prediction['class'] == 'ball':
+            ball_y = prediction['y']
+        elif prediction['class'] == 'rim':
+            rim_y = prediction['y']
+        else:
+            player_positions.append((start_x, start_y))
         cv2.rectangle(image, (start_x, start_y), (int(start_x - width), int(start_y - height)), (0, 255, 0), 2)
         cv2.putText(image, f'{prediction['class']} {str(round(prediction['confidence'], 2))}', (int(start_x - width), int(start_y - height) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     if rim_y and ball_y:
         shot = detect_shot.detect_shot(rim_y, ball_y)
-    cv2.putText(image, f'Shot: {str(shot)}', (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    cv2.imshow("Original Image with Detected Players", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-detect_players_with_roboflow('data/frame50.jpg')
+    # cv2.putText(image, f'Shot: {str(shot)}', (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # cv2.imshow("Original Image with Detected Players", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return img_str, player_positions
---
+++ modeling/plot_players.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import detect_players
+
+def plot_player_positions(img_str, player_positions):
