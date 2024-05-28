b3c0c83a464489029db28f58d5e5a530ebba41f8
artiehumphreys <artieh2003@gmail.com>
2024-05-28 10:14:17 -0400

    Made function for fetching four points of paint for homography

--- modeling/plot_players.py
+++ modeling/plot_players.py
@@ -3,8 +3,22 @@ import matplotlib.patches as patches
 from detection import detect_players as detect
 
 
+def fetch_points_for_homography(img_str):
+    lowest_point = highest_point = right_most_point = left_most_point = None
+    project_id = "basketball_court_segmentation"
+    model_id = 2
+    predictions = detect.make_request(img_str, project_id, model_id)
+    for prediction in predictions["predictions"]:
+        points = [(point["x"], point["y"]) for point in prediction["points"]]
+        if prediction["class"] == "three_second_area":
+            lowest_point = max(points, key=lambda p: p[1])
+            highest_point = min(points, key=lambda p: p[1])
+            right_most_point = max(points, key=lambda p: p[0])
+            left_most_point = min(points, key=lambda p: p[0])
+    return lowest_point, highest_point, right_most_point, left_most_point
+
+
 def plot_player_positions(img_str, player_positions):
-    lowest_point = None
     _, ax = plt.subplots(figsize=(10, 7))
     project_id = "basketball_court_segmentation"
     model_id = 2
@@ -16,11 +30,6 @@ def plot_player_positions(img_str, player_positions):
                 color = "blue"
             case "three_second_area":
                 color = "red"
-                current_lowest = max(points, key=lambda p: p[1])
-                if lowest_point is None:
-                    lowest_point = current_lowest
-                elif current_lowest[0] > lowest_point[0]:
-                    lowest_point = current_lowest
             case "court":
                 color = "green"
             case _:
@@ -35,8 +44,6 @@ def plot_player_positions(img_str, player_positions):
     ax.set_aspect("equal", adjustable="box")
     for pos in player_positions:
         ax.plot(pos[0], pos[1], "o", markersize=10, color="blue")
-    if lowest_point:
-        ax.plot(lowest_point[0], lowest_point[1], "o", markersize=10, color="red")
     plt.gca().invert_yaxis()
     # plt.legend()
     plt.title("Basketball Court Areas")
