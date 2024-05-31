fbb52068ffcd4dcc618a5e93dc25de5a1a13eedd
artiehumphreys <artieh2003@gmail.com>
2024-05-31 08:45:50 -0400

    Working on player detection using mask crnn

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from homography import calculate_points as cp
 
 def main():
     court_corners = [(190, 192.5), (310, 2.5), (310, 2.5), (190, 2.5)]
-    img_path = "data/frame30.jpg"
-    # pp.extract_frames(5)
-    decoded_img, player_positions = dp.detect_players_with_roboflow(image_path=img_path)
-    pp.plot_player_positions(decoded_img, player_positions)
-    camera_view_corners = cp.fetch_points_for_homography(decoded_img)
-    db.plot_transformed_positions(player_positions, camera_view_corners, court_corners)
+    img_path = "data/frame40.jpg"
+    # # pp.extract_frames(5)
+    # decoded_img, player_positions = dp.detect_players_with_roboflow(image_path=img_path)
+    # pp.plot_player_positions(decoded_img, player_positions)
+    # camera_view_corners = cp.fetch_points_for_homography(decoded_img)
+    # db.plot_transformed_positions(player_positions, camera_view_corners, court_corners)
+    dp.detect_players_with_mask_crnn(img_path)
 
 
 if __name__ == "__main__":
