1e9afeb3292a76c7b43c44a65552e01cf8c191eb
artiehumphreys <artieh2003@gmail.com>
2024-06-04 11:23:29 -0400

    Converted to HSL coloring. Refs and players now correctly detected. Might need to adjust color weights later

--- detection/detect_players.py
+++ detection/detect_players.py
@@ -192,32 +192,32 @@ def detect_players_with_mask_crnn(image_path: str):
 
 
 def get_teams(player_img):
-    player_hsv = cv2.cvtColor(player_img, cv2.COLOR_BGR2HSV)
-    lower_maroon = np.array([0, 31, 255])
-    upper_maroon = np.array([176, 255, 255])
+    player_hsv = cv2.cvtColor(player_img, cv2.COLOR_BGR2HLS)
+    lower_maroon = np.array([25, 125, 50])
+    upper_maroon = np.array([200, 255, 255])
 
-    lower_white = np.array([0, 0, 0])
-    upper_white = np.array([0, 0, 255])
-
+    lower_white = np.array([80, 50, 150])
+    upper_white = np.array([200, 150, 255])
+    # Check for maroon jersey
     mask_maroon = cv2.inRange(player_hsv, lower_maroon, upper_maroon)
     res_maroon = cv2.bitwise_and(player_img, player_img, mask=mask_maroon)
-    res_maroon = cv2.cvtColor(res_maroon, cv2.COLOR_HSV2BGR)
+    res_maroon = cv2.cvtColor(res_maroon, cv2.COLOR_HLS2BGR)
     res_maroon = cv2.cvtColor(res_maroon, cv2.COLOR_BGR2GRAY)
     nz_count_maroon = cv2.countNonZero(res_maroon)
 
-    # Check for white jersey
     mask_white = cv2.inRange(player_hsv, lower_white, upper_white)
     res_white = cv2.bitwise_and(player_img, player_img, mask=mask_white)
-    res_white = cv2.cvtColor(res_white, cv2.COLOR_HSV2BGR)
+    res_white = cv2.cvtColor(res_white, cv2.COLOR_HLS2BGR)
     res_white = cv2.cvtColor(res_white, cv2.COLOR_BGR2GRAY)
     nz_count_white = cv2.countNonZero(res_white)
 
-    if nz_count_maroon >= 20:
-        return "maroon"
-    elif nz_count_white >= 20:
-        return "white"
-    else:
-        return "unknown"
+    # return str(nz_count_maroon) + ", " + str(nz_count_white)
+
+    if abs(nz_count_maroon - nz_count_white) > 1500:
+        return "Referee"
+    elif nz_count_maroon > nz_count_white:
+        return "Cavs"
+    return "Bulls"
 
 
 def display_image(image):
