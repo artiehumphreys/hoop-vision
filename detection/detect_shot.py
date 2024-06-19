d274edead2dd6d8e9bcf66368fbbf994106f0e81
artiehumphreys <artieh2003@gmail.com>
2024-06-19 11:52:37 -0400

    feat: shot detector class

--- detection/detect_shot.py
+++
@@ -1,7 +0,0 @@
-# shot logic:
-# - keep track of latest player within certain distance of ball (might need another model to track ball)
-# - if ball is a certain threshold above rim, it is a shot
-def detect_shot(rim_y: int, ball_y: int, height_threshold: int = 100):
-    if ball_y < rim_y - height_threshold:
-        return True
-    return False
