e86b987cb816ba59a36d91f795b531682ceec4f5
artiehumphreys <artieh2003@gmail.com>
2024-06-28 09:05:56 -0400

    fix: variable renaming

--- detection/jersey_detector.py
+++ detection/jersey_detector.py
@@ -12,36 +12,39 @@ class JerseyDetector:
     def __init__(self, player_imgs):
         self.player_imgs = player_imgs
         self.histogram = None
-        self.peak_hues = []
+        self.peak_cr_values = []
 
     def set_player_imgs(self, player_imgs):
         self.player_imgs = player_imgs
 
     # https://cliveunger.github.io/pdfs/Basketball_Player_Tracking.pdf
-    def find_peak_hues(self):
-        peak_hues = []
+    def find_peak_cr_values(self):
+        peak_crs = []
         for player_img in self.player_imgs:
             _, cr_channel, _ = cv2.split(player_img)
             non_zero_cr = cr_channel[cr_channel > 0]
             peak_cr = np.bincount(non_zero_cr).argmax()
-            peak_hues.append(peak_cr)
-        self.peak_hues = peak_hues
+            peak_crs.append(peak_cr)
+        self.peak_cr_values = peak_crs
 
     def create_histogram(self):
-        if not self.peak_hues:
-            self.find_peak_hues()
+        if not self.peak_cr_values:
+            self.find_peak_cr_values()
 
         hist_size = 32
-        histogram, _ = np.histogram(self.peak_hues, bins=hist_size, range=(0, 256))
+        histogram, _ = np.histogram(self.peak_cr_values, bins=hist_size, range=(0, 256))
         self.histogram = histogram
 
     def get_teams(self):
         if self.histogram is None:
             self.create_histogram()
 
-        kmeans = KMeans(n_clusters=2).fit(np.array(self.peak_hues).reshape(-1, 1))
+        threshold = 12
+        kmeans = KMeans(n_clusters=2).fit(np.array(self.peak_cr_values).reshape(-1, 1))
         cluster_centers = kmeans.cluster_centers_.flatten()
-        teams_hue_ranges = [(center - 12, center + 12) for center in cluster_centers]
+        teams_hue_ranges = [
+            (center - threshold, center + threshold) for center in cluster_centers
+        ]
 
         return teams_hue_ranges
 
@@ -54,10 +57,10 @@ class JerseyDetector:
         for player_img in self.player_imgs:
             _, cr_channel, _ = cv2.split(player_img)
             non_zero_cr = cr_channel[cr_channel > 0]
-            dominant_cr = np.median(non_zero_cr)
+            median_cr = np.median(non_zero_cr)
             team = "Referee"
             for j, (low, high) in enumerate(teams_hue_ranges):
-                if low <= dominant_cr < math.floor(high):
+                if math.floor(low) <= median_cr < math.floor(high):
                     team = teams[j]
                     break
             player_teams.append(team)
